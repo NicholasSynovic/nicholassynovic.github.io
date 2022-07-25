1b19635404f7d9379e0193888abcfa5ecaafc66b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-07-25 18:04:36 -0500

    Add deployment script

---
+++ deploy.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp CNAME public/ 
+
+mkdir gopher
+find -L ./public -name "gophermap.txt" -exec rename .txt  '' {} \;
+find -L ./public -name "gophermap" -exec cp --parents {} gopher \;
+mv ./gopher/public/* ./gopher/
+rm -r ./gopher/public
+
+mkdir gemini 
+find -L ./public -name "index.gmi" -exec cp --parents {} gemini \;
+mv ./gemini/public/* ./gemini/
+rm -r ./gemini/public
