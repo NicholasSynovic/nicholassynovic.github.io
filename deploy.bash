8a4b8252f565da0211bca3527c313f99901aef5e
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-07-25 18:45:45 -0500

    Remove ./ from directories

--- deploy.bash
+++ deploy.bash
@@ -3,15 +3,15 @@
 cp CNAME public/ 
 
 mkdir gopher
-find -L ./public -name "gophermap.txt" -exec rename .txt  '' '{}' \;
-find -L ./public -name "gophermap" -exec cp --parents {} gopher \;
-mv ./gopher/public/* ./gopher/
-rm -r ./gopher/public
+find -L public -name "gophermap.txt" -exec rename .txt  '' '{}' \;
+find -L public -name "gophermap" -exec cp --parents {} gopher \;
+mv gopher/public/* gopher/
+rm -r gopher/public
 
 mkdir gemini 
-find -L ./public -name "index.gmi" -exec cp --parents {} gemini \;
-mv ./gemini/public/* ./gemini/
-rm -r ./gemini/public
+find -L public -name "index.gmi" -exec cp --parents {} gemini \;
+mv gemini/public/* gemini/
+rm -r gemini/public
 
-mv ./gemini/ ./public/
-mv ./gopher/ ./public/
+mv gemini/ public/
+mv gopher/ public/
