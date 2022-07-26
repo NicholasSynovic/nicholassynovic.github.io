331c9541e108d9ad57f54b611c3bd713e2660592
NicholasSynovic <NicholasSynovic@users.noreply.github.com>
2022-07-26 01:30:55 +0000

    deploy: b2c7cff97025cbf8c3939252d97ce617e02ab59d

--- buildGopherGemini.bash
+++ buildGopherGemini.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Script meant to be ran on a server after cloning the `gh-pages` branch of this repository
+
+rm -r gopher
+rm -r gemini
+
 mkdir gopher
 find -L . -name "gophermap.txt" -exec rename ".txt"  '' {} \;
 find -L . -name "gophermap" -exec cp --parents {} gopher \;
