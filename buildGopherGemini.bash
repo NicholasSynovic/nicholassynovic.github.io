0dcffabe696c22bc804eaa9cfa408e4282b14d64
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-07-25 19:29:35 -0500

    Remove extra folder operations

--- buildGopherGemini.bash
+++ buildGopherGemini.bash
@@ -3,10 +3,6 @@
 mkdir gopher
 find -L . -name "gophermap.txt" -exec rename ".txt"  '' {} \;
 find -L . -name "gophermap" -exec cp --parents {} gopher \;
-mv gopher/public/* gopher/
-rm -r gopher/public
 
 mkdir gemini 
 find -L . -name "index.gmi" -exec cp --parents {} gemini \;
-mv gemini/public/* gemini/
-rm -r gemini/public
