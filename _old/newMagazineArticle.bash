1a323e5ff7008d26a71a42397899f1f41ec0a0f4
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-09-01 17:00:29 -0500

    Delete old files

--- _old/newMagazineArticle.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source util/optparse.bash
-
-optparse.define short=f long=filename desc="The filename of the document to create" variable=file
-
-source $( optparse.build )
-
-hugo new $file -c ./content/summaries --kind magazine-article-summaries
